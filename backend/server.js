2a69a3ceb486d7510a19222101dcfebbfbb32b1c
shivamsaini89 <sainishivam221@gmail.com>
2025-02-22 22:33:12 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://nn-user-info-form.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 
 connectDB();
